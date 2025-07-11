54039f48b94c9a15db42df058c09421f577fb09a
omer.dartar <omerdartaar@gmail.com>
2025-07-12 02:24:38 +0300

    Added txt and py firstly

---
+++ ParsingStarWarsEpsiode.py
@@ -0,0 +1,54 @@
+# Star Wars Epsiode 3
+# Revenge Of The Stih
+
+#starwars3.txt
+
+#Senaryoda ki boşlukları temizle
+#tum satirlarin uzunluklarini cikar
+#en uzun satir kacinci sirada ve icinde ne yaziyor
+
+
+with open('starwars3.txt','r',encoding='utf-8') as f:
+    SW=f.read().splitlines()
+
+#print(SW)
+
+#print(SW[0])
+#Senaryoda ki boşlukları temizle 
+print(f'Boşluklar temizlenmeden önce uzunluk: {len(SW)}')
+while '' in SW:
+    SW.remove('')
+    
+print(f'Boşluklar temizlenmeden önce uzunluk: {len(SW)}')
+
+#Senaryoda ki boşlukları temizle OK
+
+
+
+
+#Tum satirlarin uzunluklarini cikar
+
+number = 0
+lenght= []
+for i in SW:
+    lenght.append(len(i))
+    print(f'{number}. satirin uzunlugu: {len(i)} = {i}')
+    number += 1
+print(lenght)
+
+#Tum satirlarin uzunluklarini cikar OK
+
+
+
+
+
+#en uzun satir kacinci sirada ve icinde ne yaziyor
+print(lenght.max())
+
+
+
+
+
+
+
+
